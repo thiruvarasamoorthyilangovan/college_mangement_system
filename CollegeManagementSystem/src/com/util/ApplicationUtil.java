1eb5a43c460bf2aa389f9086e758a36186f0e994
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-04 07:46:15 +0530

    Update ApplicationUtil.java

--- CollegeManagementSystem/src/com/util/ApplicationUtil.java
+++ CollegeManagementSystem/src/com/util/ApplicationUtil.java
@@ -1,5 +1,35 @@
 package com.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.exception.InvalidStudentException;
+
+import com.model.Student;
+
 public class ApplicationUtil {
+	
+	public static List<String> extractStudentDetails(String... studentDetails){
+		
+		List<String> studentDetailList = new ArrayList<String>();
+		for(String i:studentDetails) {
+			String arr[]=i.split(":");
+			try {
+				validateAdmissionId(arr[0]);
+			}catch(InvalidStudentException e){
+				System.out.println(e.getMessage());
+			}
+		}
+    	return studentDetailList;
+	}
+	
+	public static boolean validateAdmissionId(String admissionId) throws InvalidStudentException{
+		
+		if(admissionId==null) {
+			return true;
+		}else {
+			throw new InvalidStudentException("invalid admission id");
+		}
+	}
 
 }
+
