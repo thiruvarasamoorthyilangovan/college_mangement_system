1ec61b5b0a1d489f1e03dd0dc4dfc5b137b8ebca
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-07 17:31:20 +0530

    Update StudentManagement.java

--- CollegeManagementSystem/src/com/management/StudentManagement.java
+++ CollegeManagementSystem/src/com/management/StudentManagement.java
@@ -1,14 +1,11 @@
 package com.management;
 import com.model.Student;
-//import com.util.ApplicationUtil;
-
 import java.util.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-//import com.management.DBconnectionManager;
-
 public class StudentManagement {
 	
 	public int insertStudentDetails(List<Student> studentDetails) {
@@ -17,7 +14,7 @@ public class StudentManagement {
 		Connection con = null;
 		try{
 		    con = DBconnectionManager.getConnection();
-		    PreparedStatement pst = con.prepareStatement("INSERT INTO television VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+		    PreparedStatement pst = con.prepareStatement("INSERT INTO Student VALUES(?,?,?,?,?,?,?,?,?,?,?)");
 		    for(Student i:studentDetails){
 		        pst.setString(1,i.getAdmissionNumber());
 		        pst.setString(2,i.getStudentId());
@@ -32,30 +29,82 @@ public class StudentManagement {
 		        pst.setLong(11,i.getPhoneNumber());
 		        recordsAdded = recordsAdded+pst.executeUpdate();		        
 		    }
-		    }catch(SQLException e){
-		        e.printStackTrace();
+		}
+		catch(SQLException e){
+			return 0;
 		    }
-		    finally{
-		        try{con.close();}
-		        catch(Exception e){}
+		catch (ClassNotFoundException e) {
+		    return 0;			
 		    }
-		
+		finally{
+			try{
+				con.close();
+			}
+			catch(Exception e){
+		        		
+			}
+		}
 		return recordsAdded;
 	}
 	
-	public boolean updateStudentDetails() {
-		
-		return false;
+	public int updateStudentDetails(String studentId, String studentName) {
+
+		int rowsAffected = 0;
+		try{
+            Connection con = DBconnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement("update Student set Student_Name = ? where student_Id = ?");
+            pst.setString(2,studentId);
+            pst.setString(1,studentName);
+            rowsAffected = pst.executeUpdate();
+            con.close();
+            System.out.println(rowsAffected);
+            return rowsAffected;
+            
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e){
+        	e.printStackTrace();
+        }
+		return 0;
 	}
-	public boolean deleteStudentDetails() {
+	
+	public int deleteStudentDetails(String StudentId) {
 		
-		return false;
+        try{
+            Connection con = DBconnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement("delete from Student where Student_Id = ?");
+            pst.setString(1,StudentId);
+            int rowsAffected = pst.executeUpdate();
+            con.close();
+            return rowsAffected;
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+		return 0;
 	}
 	
 	public ArrayList<Student> selectStudentDetails(){
-		
-		return new ArrayList<Student>();
+
+        ArrayList<Student> list = new ArrayList<Student>();
+        try{
+        Connection con = DBconnectionManager.getConnection();
+        PreparedStatement pst = con.prepareStatement("select * from Student ");
+        ResultSet rs = pst.executeQuery();
+        while(rs.next()){
+            Student obj1 = new Student(rs.getString(1),rs.getString(2),rs.getString(3),rs.getDate(4),rs.getInt(5),rs.getInt(6),rs.getInt(7),rs.getInt(8),rs.getString(9),rs.getString(10),rs.getLong(11));
+            list.add(obj1);
+        }
+        con.close();
+        return list;
+        }catch(SQLException e){
+            return list;
+        }catch(ClassNotFoundException e){
+            return list;
+        }
 	}
 
 }
-
