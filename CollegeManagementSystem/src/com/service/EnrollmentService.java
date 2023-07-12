b8746ed1a79afbbb8fb8f5bacd9f5d2efdcd446a
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-12 17:24:23 +0530

    Update EnrollmentService.java

--- CollegeManagementSystem/src/com/service/EnrollmentService.java
+++ CollegeManagementSystem/src/com/service/EnrollmentService.java
@@ -1,5 +1,51 @@
 package com.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import com.management.EnrollmentManagement;
+import com.model.Enrollment;
+import com.util.ApplicationUtil;
+
 public class EnrollmentService {
 
+	public List<Enrollment> buildEnrollmentList(List<String> enrollmentDetails){
+		
+		List<Enrollment> enrollmentDetailList = new ArrayList<Enrollment>();
+		String DELIMETER = ":";
+		for(String i : enrollmentDetails) {
+			String arr[] = i.split(DELIMETER);
+			Enrollment obj = new Enrollment(arr[0],arr[1],arr[2],arr[3]);
+			enrollmentDetailList.add(obj);
+		}
+    	return enrollmentDetailList;
+	}
+
+    public int addEnrollment(String... enrollmentDetails){
+    	
+    	EnrollmentManagement obj = new EnrollmentManagement();
+    	int result = obj.insertEnrollmentDetails(buildEnrollmentList(ApplicationUtil.extractEnrollmentDetails(enrollmentDetails))); 
+    	return result;
+    }
+    
+    public boolean modifyEnrollment(String EnrollmentId, String EnrollmentName) {
+    	
+    	EnrollmentManagement obj = new EnrollmentManagement();
+    	int n = obj.updateEnrollmentDetails(EnrollmentId, EnrollmentName);
+    	return (n>0);
+    }
+    
+    public List<Enrollment> fetchEnrollmentDetails(String EnrollmentId){
+    	
+    	List<Enrollment> enrollmentDetailList = new ArrayList<Enrollment>();
+    	EnrollmentManagement obj = new EnrollmentManagement();
+    	enrollmentDetailList = obj.selectEnrollmentDetails(EnrollmentId);
+    	return enrollmentDetailList;
+    }
+ 
+    public boolean deleteEnrollment(String EnrollmentId) {
+    	
+    	EnrollmentManagement obj=new EnrollmentManagement();
+    	int n = obj.deleteEnrollmentDetails(EnrollmentId);
+    	return (n<0);
+    }
 }
