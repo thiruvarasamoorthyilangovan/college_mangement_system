f3b3dbc10250ad39ac053c32d617ace2934aaa5b
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-12 17:19:45 +0530

    Update EnrollmentManagement.java

--- CollegeManagementSystem/src/com/management/EnrollmentManagement.java
+++ CollegeManagementSystem/src/com/management/EnrollmentManagement.java
@@ -1,28 +1,107 @@
 package com.management;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-
+import java.util.List;
 import com.model.Enrollment;
 
 public class EnrollmentManagement {
 	
-public boolean insertEnrollmentDetails() {
+public int insertEnrollmentDetails(List<Enrollment> enrollmentDetails) {
 		
-		return false;
+	int recordsAdded = 0;
+	Connection con = null;
+	try{
+	    con = DBconnectionManager.getConnection();
+	    PreparedStatement pst = con.prepareStatement("INSERT INTO ENROLLMENT VALUES(?,?,?,?)");
+	    for(Enrollment i:enrollmentDetails){
+	        pst.setString(1,i.getEnrollmentId());
+	        pst.setString(2,i.getStudentId());
+	        pst.setString(3,i.getCourseId());
+	        pst.setString(4,i.getFeeStatus());
+	        recordsAdded = recordsAdded+pst.executeUpdate();	
+	    }
+	}
+	catch(SQLException e){
+		System.out.println(e.getMessage());
+		return 0;
+	    }
+	catch (ClassNotFoundException e) {
+		System.out.println(e.getMessage());
+		return 0;			
+	    }
+	finally{
+		try{
+			con.close();
+		}
+		catch(Exception e){
+	        		
+		}
+	}
+	return recordsAdded;
 	}
 	
-	public boolean updateEnrollmentDetails() {
+	public int updateEnrollmentDetails(String feeStatus,String enrollmentId) {
 		
-		return false;
+		int rowsAffected = 0;
+		try{
+            Connection con = DBconnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement("update ENROLLMENT set FEE_STATUS = ? where ENROLLMENT_ID = ?");
+            pst.setString(2,enrollmentId);
+            pst.setString(1,feeStatus);
+            rowsAffected = pst.executeUpdate();
+            con.close();
+            return rowsAffected;
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        catch(ClassNotFoundException e){
+        	e.printStackTrace();
+        }
+		return 0;
 	}
-	public boolean deleteEnrollmentDetails() {
+	public int deleteEnrollmentDetails(String enrollmentId) {
 		
-		return false;
+		try{
+            Connection con = DBconnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement("delete from ENROLLMENT where ENROLLMENT_ID = ?");
+            pst.setString(1,enrollmentId);
+            int rowsAffected = pst.executeUpdate();
+            con.close();
+            return rowsAffected;
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+		return 0;
 	}
 	
-	public ArrayList<Enrollment> selectEnrollmentDetails(){
+	public ArrayList<Enrollment> selectEnrollmentDetails(String enrollmentId){
 		
-		return new ArrayList<Enrollment>();
+		ArrayList<Enrollment> list = new ArrayList<Enrollment>();
+        try{
+        Connection con = DBconnectionManager.getConnection();
+        PreparedStatement pst = con.prepareStatement("select * from ENROLLMENT WHERE ENROLLMENT-ID=?");
+        pst.setString(1,enrollmentId);
+        ResultSet rs = pst.executeQuery();
+        while(rs.next()){
+        	Enrollment obj1 = new Enrollment(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4));
+            list.add(obj1);
+        }
+        con.close();
+        	return list;
+        }catch(SQLException e){
+            e.printStackTrace();
+            return list;
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+            return list;
+        }
 	}
 
 }
