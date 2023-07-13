31e2313a9c3798317f2e89b8cfd08aa3febdac3c
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-13 18:54:27 +0530

    Update ApplicationUtil.java

--- CollegeManagementSystem/src/com/util/ApplicationUtil.java
+++ CollegeManagementSystem/src/com/util/ApplicationUtil.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import com.exception.InvalidStudentException;
 
-import com.model.Student;
-
 public class ApplicationUtil {
 	
 	public static List<String> extractStudentDetails(String... studentDetails){
@@ -15,6 +13,7 @@ public class ApplicationUtil {
 			String arr[]=i.split(":");
 			try {
 				validateAdmissionId(arr[0]);
+				studentDetailList.add(i);
 			}catch(InvalidStudentException e){
 				System.out.println(e.getMessage());
 			}
@@ -24,12 +23,51 @@ public class ApplicationUtil {
 	
 	public static boolean validateAdmissionId(String admissionId) throws InvalidStudentException{
 		
-		if(admissionId==null) {
+		if(admissionId!=null) {
 			return true;
 		}else {
-			throw new InvalidStudentException("invalid admission id");
+			throw new InvalidStudentException(admissionId+" Invalid Admission Id");
 		}
 	}
+	
+	public static List<String> extractCourseDetails(String... courseDetails){
+		
+		List<String> courseDetailList = new ArrayList<String>();
+		for(String i:courseDetails) {
+			String arr[]=i.split(":");
+			if(validateCourseId(arr[0])) {
+				courseDetailList.add(i);
+			}
+			else {
+				System.out.println(arr[0]+" is invalid Course Id");
+			}
+		}
+    	return courseDetailList;
+	}
+	
+	public static boolean validateCourseId(String courseId) {
+		
+		if(courseId!=null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static List<String> extractEnrollmentDetails(String... enrollmentDetails){
+		
+		List<String> enrollmentDetailList = new ArrayList<String>();
+		for(String i:enrollmentDetails) {
+			String arr[]=i.split(":");
+			if(validateCourseId(arr[0])) {
+				enrollmentDetailList.add(i);
+			}
+			else {
+				System.out.println(arr[0]+" is invalid Course Id");
+			}
+		}
+    	return enrollmentDetailList;
+	}
+	
 
 }
 
