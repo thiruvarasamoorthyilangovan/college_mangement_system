b7b8f741fe4955d9bca17ec22ea74257c1c69dcc
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-13 18:47:57 +0530

    Update AttendanceManagement.java

--- CollegeManagementSystem/src/com/management/AttendanceManagement.java
+++ CollegeManagementSystem/src/com/management/AttendanceManagement.java
@@ -1,37 +1,88 @@
 package com.management;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
 
-public class DBconnectionManager {
+import java.util.ArrayList;
+import java.util.List;
+import com.model.Attendance;
+import java.sql.*;
 
-	private static Connection con = null;
-	private static Properties props = new Properties();
+public class AttendanceManagement {
 	
-
-    //ENSURE YOU DON'T CHANGE THE BELOW CODE WHEN YOU SUBMIT
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-	    try{
+	public int insertAttendanceDetails(List <Attendance> attendanceDetails) {
+		
+		int recordsAdded = 0;
+		Connection con = null;
+		try{
+		    con = DBconnectionManager.getConnection();
+		    PreparedStatement pst = con.prepareStatement("INSERT INTO ATTENDANCE VALUES(?,?,?,?,?,?,?,?)");
+		    for(Attendance i:attendanceDetails){
+		        pst.setString(1,i.getAttendanceId());
+		        pst.setString(2,i.getStudentId());
+		        pst.setString(3,i.getEnrollmentId());
+		        pst.setInt(4,i.getSemester());
+		        pst.setInt(5,i.getToatalWorkingDays());
+		        pst.setInt(6,i.getPresentDays());
+		        pst.setInt(7,i.getAbsentDays());
+		        pst.setInt(8,i.getAttendancePercentage());
+		        recordsAdded = recordsAdded+pst.executeUpdate();	
+		    }
+		}
+		catch(SQLException e){
+			return 0;
+		    }
+		catch (ClassNotFoundException e) {
+		    return 0;			
+		    }
+		finally{
+			try{
+				con.close();
+			}
+			catch(Exception e){
+		        		
+			}
+		}
+		return recordsAdded;
+	}
+	public ArrayList<Attendance> selectAttendanceDetails(String studentId){
+		
+		ArrayList<Attendance> attendanceList = new ArrayList<Attendance>();
+		try {
+			Connection con = DBconnectionManager.getConnection() ;
+			PreparedStatement pst = con.prepareStatement("SELECT * FORM ATTENDANCE WHERE STUDENT_ID = ?");
+			pst.setString(1, studentId);
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()) {
+				Attendance attendance = new Attendance(rs.getString(1),rs.getString(2), rs.getString(3),rs.getInt(4), rs.getInt(5), rs.getInt(6), rs.getInt(7), rs.getInt(8));
+				attendanceList.add(attendance);
+			}
 			
-			FileInputStream fis = null;
-			fis = new FileInputStream("database.properties");
-			props.load(fis);
-			
-			// load the Driver Class
-			Class.forName(props.getProperty("DB_DRIVER_CLASS"));
+		}catch(SQLException e) {
+			return attendanceList;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return attendanceList;
+	}
+	public int updateAttendanceDetails(String updateColumn, String updateValue, String referenceColumn, String referenceValue) {
 
-			// create the connection now
-            con = DriverManager.getConnection(props.getProperty("DB_URL"),props.getProperty("DB_USERNAME"),props.getProperty("DB_PASSWORD"));
-	    }
-	    catch(IOException e){
-	        e.printStackTrace();
-	    }
-		return con;	
+		int rowsAffected = 0;
+		try{
+            Connection con = DBconnectionManager.getConnection();
+            PreparedStatement pst = con.prepareStatement("update Attendance set ?? = ? where ?? = ?");
+            pst.setString(1,updateColumn);
+            pst.setString(2,updateValue);
+            pst.setString(3,referenceColumn);
+            pst.setString(4,referenceValue);
+            rowsAffected = pst.executeUpdate();
+            con.close();
+            return rowsAffected;
+            
+        }
+        catch(SQLException e){
+        	System.out.println(e.getMessage());
+        }
+        catch(ClassNotFoundException e){
+        	System.out.println(e.getMessage());
+        }
+		return 0;
 	}
 }
-	 	  	 	      	     	      	 	   	 	
-
-	
