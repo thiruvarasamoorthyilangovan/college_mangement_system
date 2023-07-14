3538e08f577a2d31f6fae75a72de50f4b3fa9c9c
thiruvarasamoorthyilangovan <127823718+thiruvarasamoorthyilangovan@users.noreply.github.com>
2023-07-14 10:56:29 +0530

    Update ApplicationUtil.java

--- CollegeManagementSystem/src/com/util/ApplicationUtil.java
+++ CollegeManagementSystem/src/com/util/ApplicationUtil.java
@@ -2,6 +2,8 @@ package com.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
 import com.exception.InvalidStudentException;
 
 public class ApplicationUtil {
@@ -9,8 +11,9 @@ public class ApplicationUtil {
 	public static List<String> extractStudentDetails(String... studentDetails){
 		
 		List<String> studentDetailList = new ArrayList<String>();
+		String DELIMITER = ":";
 		for(String i:studentDetails) {
-			String arr[]=i.split(":");
+			String arr[]=i.split(DELIMITER);
 			try {
 				validateAdmissionId(arr[0]);
 				studentDetailList.add(i);
@@ -23,7 +26,7 @@ public class ApplicationUtil {
 	
 	public static boolean validateAdmissionId(String admissionId) throws InvalidStudentException{
 		
-		if(admissionId!=null) {
+		if(Pattern.matches("AD20[1-2]{1}[1-9]{1}-00[0-9]", admissionId)) {
 			return true;
 		}else {
 			throw new InvalidStudentException(admissionId+" Invalid Admission Id");
@@ -33,8 +36,9 @@ public class ApplicationUtil {
 	public static List<String> extractCourseDetails(String... courseDetails){
 		
 		List<String> courseDetailList = new ArrayList<String>();
+		String DELIMITER = ":";
 		for(String i:courseDetails) {
-			String arr[]=i.split(":");
+			String arr[]=i.split(DELIMITER);
 			if(validateCourseId(arr[0])) {
 				courseDetailList.add(i);
 			}
@@ -47,7 +51,7 @@ public class ApplicationUtil {
 	
 	public static boolean validateCourseId(String courseId) {
 		
-		if(courseId!=null) {
+		if(Pattern.matches("[A-Z]00[0-9]", courseId)) {
 			return true;
 		}else {
 			return false;
@@ -56,16 +60,25 @@ public class ApplicationUtil {
 	public static List<String> extractEnrollmentDetails(String... enrollmentDetails){
 		
 		List<String> enrollmentDetailList = new ArrayList<String>();
+		String DELIMITER = ":";
 		for(String i:enrollmentDetails) {
-			String arr[]=i.split(":");
-			if(validateCourseId(arr[0])) {
+			String arr[]=i.split(DELIMITER);
+			if(validateEnrollmentId(arr[0])) {
 				enrollmentDetailList.add(i);
 			}
 			else {
-				System.out.println(arr[0]+" is invalid Course Id");
+				System.out.println(arr[0]+" is invalid Enrollment Id");
 			}
 		}
     	return enrollmentDetailList;
+	}
+	public static boolean validateEnrollmentId(String enrollmentId) {
+		
+		if(Pattern.matches("EM20[1-2]{1}[1-9]{1}-00[0-9]", enrollmentId)) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 
